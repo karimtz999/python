1bec8faaad105ad8870d702beb471984d386315a
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-02-01 23:48:15 +0100

    Create Line_Width.py

---
+++ Line_Width.py
@@ -0,0 +1,16 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linewidth = '20.5')
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
