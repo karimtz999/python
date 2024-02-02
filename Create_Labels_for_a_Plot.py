f3d8f644944e4d6e76ce6bf0623ce1c63000ca38
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-02-02 23:58:44 +0100

    Create Create_Labels_for_a_Plot.py

---
+++ Create_Labels_for_a_Plot.py
@@ -0,0 +1,21 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show() 
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
