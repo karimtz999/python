fd51a70dfc56586e90d44b60484e5390438e6f9b
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-02-04 14:13:13 +0100

    Create Legend.py

---
+++ Legend.py
@@ -0,0 +1,18 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels = mylabels)
+plt.legend()
+plt.show() 
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
