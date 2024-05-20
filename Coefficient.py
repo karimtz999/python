f664d22d6a7b371509f05458480c91f539503a09
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-05-20 23:58:11 +0100

    Create Coefficient.py

---
+++ Coefficient.py
@@ -0,0 +1,13 @@
+import numpy
+from sklearn import linear_model
+
+X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+logr = linear_model.LogisticRegression()
+logr.fit(X,y)
+
+log_odds = logr.coef_ 
+odds = numpy.exp(log_odds)
+
+print(odds)
